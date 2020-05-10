20bc9063b932f1d0feecd778d7ff5c1fe6c6fd34
Erivan Junior <silva.erivanjr@yahoo.com.br>
2020-05-10 03:58:37 -0300

    Adição das rotas

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './pages/Home';
+import Country from './pages/Country';
 
 const AppStack = createStackNavigator();
 
@@ -11,7 +12,8 @@ export default function Routes() {
     return (
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{ headerShown: false }} >
-                <AppStack.Screen name="Home" component={Home} /> 
+                <AppStack.Screen name="Home" component={Home} />
+                <AppStack.Screen name="Details" component={Country} /> 
             </AppStack.Navigator>
         </NavigationContainer>
     );
