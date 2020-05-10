28514f77b31d88deb1e73cb091b5086206ae8aee
Erivan Junior <silva.erivanjr@yahoo.com.br>
2020-05-10 03:59:18 -0300

    Estrutura e css básico de Country Page

---
+++ src/pages/Country/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, TouchableOpacity} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+import styles from './styles';
+
+export default function Country() {
+
+    const route = useRoute();
+    const country = route.params.country;
+    const navigation = useNavigation();
+
+    function navigateBack() {
+        navigation.goBack();
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.detailsButton}>
+                <TouchableOpacity
+                    style={styles.detailsButtonText}
+                    onPress={navigateBack}>
+                </TouchableOpacity>
+            </View>
+    <Text style={styles.title}>{country}</Text>
+        </View>
+    );
+}
