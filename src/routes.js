c52dec45dd73fd36d64b53e15583a7c4955e87af
Erivan Junior <silva.erivanjr@yahoo.com.br>
2020-05-17 01:11:45 -0300

    Criação da rotas

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './pages/Home';
+import List from './pages/List';
 import Country from './pages/Country';
+import TopFive from './pages/TopFive';
+import Tips from './pages/Tips';
 
 const AppStack = createStackNavigator();
 
@@ -13,6 +16,9 @@ export default function Routes() {
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{ headerShown: false }} >
                 <AppStack.Screen name="Home" component={Home} />
+                <AppStack.Screen name="List" component={List} />
+                <AppStack.Screen name="TopFive" component={TopFive} />
+                <AppStack.Screen name="Tips" component={Tips} />
                 <AppStack.Screen name="Details" component={Country} /> 
             </AppStack.Navigator>
         </NavigationContainer>
